0c57d735d0b6484b3b5d705bc78b071fe2bfb053
jhocardoso <jhonatancardoso17@hotmail.com>
2021-02-17 01:18:10 +0000

    (16/02) Continuação da revisão de JS

---
+++ revisao-js/includes/funcoes2.mjs
@@ -0,0 +1,10 @@
+// Neste arquivo, há apenas um item sendo exportado, e é uma function.
+// Podemos usar o export default
+
+export default function dobraVet(vet) {
+    // Cria um novo vetor com elementos que são o dobro do valor 
+    // do vetor original
+    let res = []
+    for(let n of vet) res.push(n * 2)
+    return res
+}
---
+++ revisao-js/operador-ternario.js
@@ -0,0 +1,31 @@
+let nota = 7.2
+let situacao
+
+/*if (nota >= 6) {
+    situacao = 'APROVADO'
+}
+else {
+    situacao = 'REPROVADO'
+}*/
+
+/*if (nota >= 6) situacao = 'APROVADO'
+else situacao = 'REPROVADO'*/
+
+nota = 5.3
+
+// As três partes do operador ternário
+// 1ª parte: condição (que iria depois do if)
+// Entre a 1ª e a 2ª parte -> ?
+// 2ª parte: o resultado, caso a condição seja VERDADEIRA
+// Entre a 2ª e a 3ª parte -> :
+// 3ª parte: o resultado, caso a condição seja FALSA
+situacao = (nota >= 6 ? 'APROVADO' : 'REPROVADO')
+
+console.log(situacao)
+
+let user = 'admin'
+let userType
+
+userType = user === 'admin' || user === 'root' ? 'Superuser' : 'Ordinary user'
+
+console.log(userType)
